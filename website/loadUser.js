4f8107fded210c5541d3805f70d164c410616871
piyuvi <31787390+piyuvi@users.noreply.github.com>
2017-09-09 08:37:24 +0000

    Add files via upload

--- website/loadUser.js
+++ website/loadUser.js
@@ -2,7 +2,7 @@
 
 
 function loadUser() {
-	$.getJSON('/data.json', function(data) {
+	$.getJSON('data.json', function(data) {
 			    var template = $('#template').html();
 			    var info = Mustache.to_html(template, data);
 			    $('#target').html(info);
